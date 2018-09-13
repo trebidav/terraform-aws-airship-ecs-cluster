d595fbf2bc661a1f160c1a03cc22f15f05ba4050
maartenvanderhoef <maarten@blinkist.com>
2018-09-13 16:03:15 +0200

    with maybe not really working splat

--- modules/autoscalinggroup/outputs.tf
+++ modules/autoscalinggroup/outputs.tf
@@ -1,4 +1,4 @@
 output "asg_name" {
   description = "The name of the autoscaling group"
-  value       = "${element(concat(aws_autoscaling_group.this.*.name, list("")), 0)}"
+  value       = "${element(concat(aws_autoscaling_group.this.*.name, aws_cloudformation_stack.autoscaling_group.*.outputs["AsgName"] list("")), 0)}"
 }
