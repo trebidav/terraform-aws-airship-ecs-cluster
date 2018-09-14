895a78a313c85bde31782225769826d7619d8df3
maartenvanderhoef <maarten@blinkist.com>
2018-09-14 10:11:25 +0200

    ..

--- modules/autoscalinggroup/outputs.tf
+++ modules/autoscalinggroup/outputs.tf
@@ -4,7 +4,6 @@ output "asg_name" {
   # With autoscalinggroup_type being MIGRATION, we have TWO Autoscaling Groups next to each other. 
   # We output "" in case of MIGRATION
 
-  value = "${local.autoscalinggroup_type == "MIGRATION" ? ""
-                     "Not asg name output supported when migrating." : 
+  value = "${local.autoscalinggroup_type == "MIGRATION" ? "" :
                       element(concat(aws_autoscaling_group.this.*.name, aws_cloudformation_stack.autoscaling_group.*.outputs["AsgName"] list("")), 0)}"
 }
